83af9a546703e7996f8afc43e228d9ded1eafcf6
shivanshkudiyal <shivanshu.29430@stu.upes.ac.in>
2025-11-25 15:13:59 +0530

    Refactor program to calculate circle properties

--- day2.2.c
+++ day2.2.c
@@ -1,4 +1,4 @@
-//Write a program to calculate the area and circumference of a circle given its radius.
+//Q4.Write a program to calculate the area and circumference of a circle given its radius.
 #include <stdio.h>
 
 int main() {
@@ -15,4 +15,5 @@ int main() {
 
     return 0;                             
 }
+
 
