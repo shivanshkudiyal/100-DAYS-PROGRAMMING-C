bda61ff35032ff76ca855c3886fa6e9ddfcf606e
shivanshkudiyal <shivanshu.29430@stu.upes.ac.in>
2025-11-25 15:14:36 +0530

    Duplicate code for arithmetic operations removed

--- day1.2.c
+++ day1.2.c
@@ -1,4 +1,4 @@
-//Write a program to input two numbers and display their sum, difference, product, and quotient.
+//Q2.Write a program to input two numbers and display their sum, difference, product, and quotient.
 
 #include <stdio.h>
 
@@ -19,4 +19,5 @@ int main() {
 
     return 0;                       
 }
+
 
