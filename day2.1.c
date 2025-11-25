e5fec876f31c15427235e7cb0a31419e3dee0769
shivanshkudiyal <shivanshu.29430@stu.upes.ac.in>
2025-11-25 15:14:17 +0530

    Update day2.1.c

--- day2.1.c
+++ day2.1.c
@@ -1,4 +1,4 @@
-//Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+//Q3.Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
 #include <stdio.h>
 
 int main() {
@@ -15,4 +15,5 @@ int main() {
 
     return 0;                          
 }
+
 
